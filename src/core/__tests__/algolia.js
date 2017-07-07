47f71edbd7ba843fd5ba378bc766e0a0a769e132
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-07-07 11:04:22 +0200

    feat(algolia): add function for handle error

--- src/core/__tests__/algolia.js
+++ src/core/__tests__/algolia.js
@@ -1,5 +1,5 @@
 import algoliasearch from 'algoliasearch';
-import client from '../algolia';
+import client, * as module from '../algolia';
 
 jest.mock('algoliasearch', () => jest.fn(() => ({
   fakeApi: 'sample',
@@ -11,12 +11,39 @@ jest.mock('configuration', () => ({
 }));
 
 describe('algolia', () => {
-  it('expect to return an instance of algolia client', () => {
-    const expectation = {
-      fakeApi: 'sample',
-    };
+  describe('singleton', () => {
+    it('expect to return an instance of algolia client', () => {
+      const expectation = {
+        fakeApi: 'sample',
+      };
 
-    expect(algoliasearch).toHaveBeenCalledWith('APP_ID', 'API_KEY');
-    expect(client).toEqual(expectation);
+      expect(algoliasearch).toHaveBeenCalledWith('APP_ID', 'API_KEY');
+      expect(client).toEqual(expectation);
+    });
+  });
+
+  describe('handleError', () => {
+    it('expect to close the client and return normalize error', () => {
+      expect.assertions(2);
+
+      const client = {
+        destroy: jest.fn(),
+      };
+
+      const error = {
+        name: 'AlgoliaUnknownError',
+        message: 'Oops, an error has occured',
+      };
+
+      const expectation = {
+        name: 'AlgoliaError',
+        details: error,
+      };
+
+      return module.handleError(client, error).catch(actual => {
+        expect(actual).toEqual(expectation);
+        expect(client.destroy).toHaveBeenCalled();
+      });
+    });
   });
 });
--- src/core/algolia.js
+++ src/core/algolia.js
@@ -1,4 +1,13 @@
 import algoliasearch from 'algoliasearch';
 import { ALGOLIA_APP_ID, ALGOLIA_API_KEY } from 'configuration';
 
+export const handleError = (client, error) => {
+  client.destroy();
+
+  return Promise.reject({
+    name: 'AlgoliaError',
+    details: error,
+  });
+};
+
 export default algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
