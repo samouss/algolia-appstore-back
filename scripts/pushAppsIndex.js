e9e2604aac5610960f90dd609842ed8d661d5129
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-07-12 21:16:34 +0200

    refactor(pushAppsIndex): update index name

--- scripts/pushAppsIndex.js
+++ scripts/pushAppsIndex.js
@@ -9,7 +9,7 @@ const { input } = yargs.argv;
 
 const appId = process.env.ALGOLIA_APP_ID || 'APP_ID';
 const apiKey = process.env.ALGOLIA_API_KEY || 'API_KEY';
-const indexName = 'apps';
+const appsIndexDesc = 'apps_rating_desc';
 
 // Input validation
 if (!input) {
@@ -23,23 +23,23 @@ if (!fs.existsSync(input)) {
 const inputPath = path.resolve(input);
 const data = JSON.parse(fs.readFileSync(inputPath));
 const client = algoliasearch(appId, apiKey);
-const index = client.initIndex(indexName);
+const index = client.initIndex(appsIndexDesc);
 
 console.log();
 console.log(chalk`{blue > Progress!}`);
-console.log(chalk`> Upload of "{bold ${indexName}}" begin...`);
+console.log(chalk`> Upload of "{bold ${appsIndexDesc}}" begin...`);
 console.log();
 
 index.addObjects(data).then(() => {
   console.log();
   console.log(chalk`{green > Success!}`);
-  console.log(chalk`> Records of "{bold ${indexName}}" will change in couple of seconds.`);
+  console.log(chalk`> Records of "{bold ${appsIndexDesc}}" will change in couple of seconds.`);
   console.log(`> ${inputPath}`);
   console.log();
 }).catch(error => {
   console.log();
   console.log(chalk`{red > Error!}`);
-  console.log(chalk`> Oops, an error has occurred during upload of "{bold ${indexName}}".`);
+  console.log(chalk`> Oops, an error has occurred during upload of "{bold ${appsIndexDesc}}".`);
   console.log();
   console.log(error);
   console.log();
